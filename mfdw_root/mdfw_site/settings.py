270263a2e0fffce3ab7d2c7d713b0ddfb7f4b9bb
juanesc1213 <j.corrales@utp.edu.co>
2022-09-30 11:16:44 -0500

    modificado settings y creado carpeta template

--- mfdw_root/mdfw_site/settings.py
+++ mfdw_root/mdfw_site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mdfw_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'mdfw_site/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'mdfw_site/static']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
