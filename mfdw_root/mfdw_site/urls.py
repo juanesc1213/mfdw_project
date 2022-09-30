31394b5c743a4c7069eff82d84e6ee94808d4db4
juanesc1213 <j.corrales@utp.edu.co>
2022-09-30 14:36:52 -0500

    modificacion funcional viernes 2:36 pm

--- mfdw_root/mfdw_site/urls.py
+++ mfdw_root/mfdw_site/urls.py
@@ -1,7 +1,7 @@
-"""mdfw_site URL Configuration
+"""mfdw_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
+from quotes.views import Register
 
 urlpatterns = [
+    # path('testpage', TemplateView.as_view(template_name='pages/page.html')),
     path('admin/', admin.site.urls),
+    path('register/success/',TemplateView.as_view(template_name="registration/success.html"), name ='register-success'),
+    path('register/', Register.as_view(), name='register'),
+    path('quote/', include('quotes.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
 ]
