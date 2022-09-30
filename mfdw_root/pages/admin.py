8928890e0142ca0324c25508b8b8a3220c1a744a
juanesc1213 <j.corrales@utp.edu.co>
2022-09-30 14:09:50 -0500

    modificado pegado del codigo original

--- mfdw_root/pages/admin.py
+++ mfdw_root/pages/admin.py
@@ -1,7 +1,10 @@
-from re import A
 from django.contrib import admin
 from .models import Page
 
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','update_date')
+    ordering = ('title',)
+    search_fields = ('title',)
 
-# Register your models here.
+admin.site.register(Page, PageAdmin)
+
--- mfdw_root/pages/models.py
+++ mfdw_root/pages/models.py
@@ -4,7 +4,7 @@ class Page(models.Model):
     title = models.CharField(max_length=60)
     permalink = models.CharField(max_length=12, unique=True)
     update_date = models.DateTimeField('Last Updated')
-    bodytext = models.TextField('Pages Content', blank=True)
+    bodytext = models.TextField('Page Content', blank=True)
 
-    def __str__(self): 
+    def __str__(self):
         return self.title
