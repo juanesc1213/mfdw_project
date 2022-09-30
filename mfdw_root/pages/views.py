f7c58c2bfe7347fb6464d4e4c5f8ac91c96e5132
juanesc1213 <j.corrales@utp.edu.co>
2022-09-30 11:22:30 -0500

    modificado views.py

--- mfdw_root/pages/views.py
+++ mfdw_root/pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>The Meandco Homepage</h1>")
+    #return HttpResponse("<h1>The Meandco Homepage</h1>")
+    return render(request, 'base.html')
 
 # Create your views here.
